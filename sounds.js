e023a2df627118778bc90e1e5503df81fa1a4f98
ronskie69 <90790271+ronskie69@users.noreply.github.com>
2021-09-30 21:37:52 +0800

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,12 @@
+export const laugh = {
+    //url: 'https://github.com/ronskie69/music/blob/main/Hysterical%20Laugh%20Sound%20Effect.mp3?raw=true',
+    url: 'https://github.com/ronskie69/music/blob/main/mali.wav?raw=true',
+}
+
+export const applause = {
+    url: 'https://github.com/ronskie69/music/blob/main/who_wants.mp3?raw=true',
+}
+
+export const bgSound = {
+    url: 'https://github.com/ronskie69/music/blob/main/Who%20Wants%20To%20Be%20A%20Millionaire_%20Phone%20A%20Friend%20Clock%20(2011-%D0%BD.%D0%B4).mp3?raw=true',
+}
